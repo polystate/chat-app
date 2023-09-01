d1847e38cb5faeafb8107b7327a6e8f6733c5506
unknown <dockadams333@gmail.com>
2023-09-01 19:08:51 +0700

    Timestamp and enter-submit message added.

--- public/script.js
+++ public/script.js
@@ -8,6 +8,7 @@ const enterUserName = document.querySelectorAll(".user-input")[0];
 const emoticon = document.getElementById("emoticon");
 const chatWindow = document.querySelector(".chat-window");
 const messagesContainer = document.createElement("div");
+let userInput = document.querySelectorAll(".user-input")[1];
 messagesContainer.setAttribute("class", "messages-container");
 chatWindow.appendChild(messagesContainer);
 
@@ -18,22 +19,22 @@ let menuOpen = false;
 //turn socket.on to listen for 'message' event emitted from server
 
 socket.on("message", (data) => {
-  const { name, message } = data;
-  outputMessage(name, message);
+  const { name, message, date } = data;
+  outputMessage(name, message, date);
 });
 
 //create event listener for the send button that makes it so socket.io emits the 'message' event and passes it to the server. this is stage 1 of socket.io ball. empty userInput value and focus() it.
 
 btnSend.addEventListener("click", (e) => {
   e.preventDefault();
+  sendMessage();
+});
 
-  let userInput = document.querySelectorAll(".user-input")[1];
-  if (!userInput.value) return;
-
-  socket.emit("message", { name: userName, message: userInput.value });
-
-  userInput.value = "";
-  userInput.focus();
+userInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    sendMessage();
+  }
 });
 
 //create an event listener for when you type into the enter user name input and it reacts immediately with input even handler. current name updates the global userName variable
@@ -56,15 +57,63 @@ emoticon.addEventListener("click", () => {
   }
 });
 
+//transfer message between server/client
+
+function sendMessage() {
+  if (!userInput.value) return;
+
+  socket.emit("message", {
+    name: userName,
+    message: userInput.value,
+    date: formatDate(new Date()),
+  });
+
+  userInput.value = "";
+  userInput.focus();
+}
+
 //dynamic output message function that creates html and sets up the structure
 
-export default function outputMessage(name, message) {
+function outputMessage(name, message, date) {
   messagesContainer.setAttribute("class", "message-container");
   const textDiv = document.createElement("div");
   textDiv.setAttribute("class", "text-container");
+  const currentDate = document.createElement("p");
+  currentDate.setAttribute("class", "current-date");
+  currentDate.textContent = date;
   const para = document.createElement("p");
   para.setAttribute("class", "message");
   para.textContent = (name || "Anon") + ": " + message;
+  textDiv.appendChild(currentDate);
   textDiv.appendChild(para);
   messagesContainer.appendChild(textDiv);
+}
+
+function formatDate(date) {
+  const months = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+  ];
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "P.M." : "A.M.";
+  const formattedHours = hours % 12 || 12;
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month}/${day}/${year} - ${formattedHours}:${
+    minutes < 10 ? "0" : ""
+  }${minutes} ${ampm}`;
 }
